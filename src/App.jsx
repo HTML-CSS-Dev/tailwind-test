fb510ee7dd8996a5743a67e89196d5d7c8579ceb
HTML-CSS-Dev <ranaldu456@gmail.com>
2026-04-18 18:09:27 +0500

    chore: move components to App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Header from "./components/Header/header";
+import Main from "./components/Main/_main";
+import ExpertiseSection from "./components/Section/expertise_section";
+import ServiceSection from "./components/Section/service_section";
+import TestiomonialSection from './components/Section/testimonials_section';
+import BlogSection from './components/Section/blog_section';
+import Footer from "./components/Footer/footer";
+
+export default function App() {
+    return (
+        <>
+            <Header />
+            <Main />
+            <ExpertiseSection />
+            <ServiceSection />
+            <TestiomonialSection />
+            <BlogSection />
+            <Footer />
+        </>
+    )
+}
