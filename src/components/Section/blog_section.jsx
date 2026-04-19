f9fa9cee8a79094400e4c80fbc4638de4211e78d
HTML-CSS-Dev <ranaldu456@gmail.com>
2026-04-19 15:07:34 +0500

    edited: edited icons insections

--- src/components/Section/blog_section.jsx
+++ src/components/Section/blog_section.jsx
@@ -1,4 +1,23 @@
 export default function BlogSection() {
+    const IMAGES = [ 
+        {   
+            img_url:"stuff1.png",
+            title:"stuff1",
+            date: "Jan 19, 2021",
+            description1:"Today’s best design",
+            description2:"trends for digital",
+            description3:"products",
+        },
+        {
+            img_url:"stuff2.png",
+            title:"stuff2",
+            date: "Jan 19, 2021",
+            description1:"A practical guide to",
+            description2:"building a brand ",
+            description3:"strategy",
+        }
+    ];
+
     return (
         <section className="bg-[#ECF8F9] py-24">
             <div className="main-container">
@@ -6,12 +25,14 @@ export default function BlogSection() {
                 <h4 className="heading mt-4.75 mb-12">Read our latest blogs & news</h4>
                 
                 <div className="flex justify-between">
+                    
+                    {IMAGES.map(
+                        img => 
                     <div className="flex">
-                        <img src="/stuff1.png" alt="stuff1" />
+                        <img src={img.img_url} alt={img.title} />
                         <div className="bg-[#FFFFFF] p-8">
-                            <span className="font-medium text-[14px] leading-5 text-[#232536]">Jan 19, 2021</span>
-                            <h3 className="font-semibold text-2xl leading-9 text-[#232536] mt-2 mb-5">Today’s best design <br /> trends for digital <br /> products</h3>
-                            
+                            <span className="font-medium text-[14px] leading-5 text-[#232536]">{img.date}</span>
+                            <h3 className="font-semibold text-2xl leading-9 text-[#232536] mt-2 mb-5">{img.description1}<br />{img.description2}<br />{img.description3}</h3>
                             
                     <h4 className="flex items-center gap-3.5 font-medium leading-6 text-[#444CFC] cursor-pointer">Read More
                         <svg className="mt-1" width="25" height="8" viewBox="0 0 25 8" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -21,23 +42,7 @@ export default function BlogSection() {
 
                         </div>
                     </div>
-
-
-                    <div className="flex">
-                        <img src="/stuff2.png" alt="stuff2" />
-                        <div className="bg-[#FFFFFF] p-8">
-                            <span className="font-medium text-[14px] leading-5 text-[#232536]">Jan 19, 2021</span>
-                            <h3 className="font-semibold text-2xl leading-9 text-[#232536] mt-2 mb-5">A practical guide to <br /> building a brand <br /> strategy</h3>
-                            
-                            
-                    <h4 className="flex items-center gap-3.5 font-medium leading-6 text-[#444CFC] cursor-pointer">Read More
-                        <svg className="mt-1" width="25" height="8" viewBox="0 0 25 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M24.3536 4.03556C24.5488 3.8403 24.5488 3.52372 24.3536 3.32846L21.1716 0.146475C20.9763 -0.0487874 20.6597 -0.0487875 20.4645 0.146475C20.2692 0.341737 20.2692 0.658319 20.4645 0.853582L23.2929 3.68201L20.4645 6.51044C20.2692 6.7057 20.2692 7.02228 20.4645 7.21754C20.6597 7.4128 20.9763 7.4128 21.1716 7.21754L24.3536 4.03556ZM-4.37114e-08 4.18201L24 4.18201L24 3.18201L4.37114e-08 3.18201L-4.37114e-08 4.18201Z" fill="black"/>
-                        </svg>
-                    </h4>
-
-                        </div>
-                    </div>
+                    )}
                 </div>
             </div>
         </section>
