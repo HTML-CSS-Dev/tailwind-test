08e76aff686c92eeb8bb7320387095f6955c37b8
HTML-CSS-Dev <ranaldu456@gmail.com>
2026-04-19 14:54:21 +0500

    edit: replaced icons

---
+++ src/common/pagination.jsx
@@ -0,0 +1,9 @@
+export default function Pagination() {
+    return (
+        <svg className="mt-6 absolute right-57.5" width="53" height="12" viewBox="0 0 53 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="6" cy="6" r="6" fill="#232536"/>
+            <circle opacity="0.2" cx="28" cy="6" r="5" fill="#232536"/>
+            <circle opacity="0.2" cx="48" cy="6" r="5" fill="#232536"/>
+        </svg>
+    )
+}
